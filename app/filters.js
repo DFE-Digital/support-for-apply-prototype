19a607a8c6a92beb3995e459f3731f1e89a55300
Adam Silver <adambsilver@gmail.com>
2019-11-12 15:55:56 +0000

    Timeline for application history

--- app/filters.js
+++ app/filters.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+
 module.exports = function (env) {
   /**
    * Instantiate object used to store the methods registered as a
@@ -37,6 +39,50 @@ module.exports = function (env) {
     documentation.
 
   ------------------------------------------------------------------ */
+
+
+  filters.date = function(timestamp, format) {
+    return moment(timestamp).format(format);
+  }
+
+  /* ------------------------------------------------------------------
+    utility functions for use in mojDate function/filter
+  ------------------------------------------------------------------ */
+  function govDate(timestamp) {
+    return moment(timestamp).format('D MMMM YYYY');
+  }
+
+  function govShortDate(timestamp) {
+    return moment(timestamp).format('D MMM YYYY');
+  }
+
+  function govTime(timestamp) {
+    let t = moment(timestamp);
+    if(t.minutes() > 0) {
+      return t.format('h:mma');
+    } else {
+      return t.format('ha');
+    }
+  }
+
+  filters.appDate = function(timestamp, type) {
+
+    switch(type) {
+      case "datetime":
+        return govDate(timestamp) + " at " + govTime(timestamp);
+      case "shortdatetime":
+        return govShortDate(timestamp) + " at " + govTime(timestamp);
+      case "date":
+        return govDate(timestamp);
+      case "shortdate":
+        return govShortDate(timestamp);
+      case "time":
+        return govTime(timestamp);
+      default:
+        return timestamp;
+    }
+
+  }
 
   /* ------------------------------------------------------------------
     keep the following line to return your filters to the app
