3402fc84d29026130b6b8a0e26ddd4efac2d5f27
Simon Whatley <simonwhatley@gmail.com>
2021-03-29 20:50:32 +0100

    Tidy up components

--- app/views/_components/button-menu/button-menu.js
+++ app/views/_components/button-menu/button-menu.js
@@ -1,6 +1,6 @@
-MOJFrontend.ButtonMenu = function(params) {
+AppFrontend.ButtonMenu = function(params) {
 	this.container = params.container;
-	this.menu = this.container.find('.moj-button-menu__wrapper');
+	this.menu = this.container.find('.app-button-menu__wrapper');
 	if(params.menuClasses) {
 		this.menu.addClass(params.menuClasses);
 	}
@@ -15,25 +15,25 @@ MOJFrontend.ButtonMenu = function(params) {
 	$(document).on('click', $.proxy(this, 'onDocumentClick'));
 };
 
-MOJFrontend.ButtonMenu.prototype.onDocumentClick = function(e) {
+AppFrontend.ButtonMenu.prototype.onDocumentClick = function(e) {
 	if(!$.contains(this.container[0], e.target)) {
 	  this.hideMenu();
   }
 };
 
-MOJFrontend.ButtonMenu.prototype.createToggleButton = function() {
-	this.menuButton = $('<button class="govuk-button moj-button-menu__toggle-button ' + this.buttonClasses + '" type="button" aria-haspopup="true" aria-expanded="false">'+this.buttonText+'</button>');
+AppFrontend.ButtonMenu.prototype.createToggleButton = function() {
+	this.menuButton = $('<button class="govuk-button app-button-menu__toggle-button ' + this.buttonClasses + '" type="button" aria-haspopup="true" aria-expanded="false">'+this.buttonText+'</button>');
 	this.menuButton.on('click', $.proxy(this, 'onMenuButtonClick'));
 	this.menuButton.on('keydown', $.proxy(this, 'onMenuKeyDown'));
 };
 
-MOJFrontend.ButtonMenu.prototype.setupResponsiveChecks = function() {
+AppFrontend.ButtonMenu.prototype.setupResponsiveChecks = function() {
 	this.mql = window.matchMedia(this.mq);
 	this.mql.addListener($.proxy(this, 'checkMode'));
 	this.checkMode(this.mql);
 };
 
-MOJFrontend.ButtonMenu.prototype.checkMode = function(mql) {
+AppFrontend.ButtonMenu.prototype.checkMode = function(mql) {
 	if(mql.matches) {
 		this.enableBigMode();
 	} else {
@@ -41,24 +41,24 @@ MOJFrontend.ButtonMenu.prototype.checkMode = function(mql) {
 	}
 };
 
-MOJFrontend.ButtonMenu.prototype.enableSmallMode = function() {
+AppFrontend.ButtonMenu.prototype.enableSmallMode = function() {
 	this.container.prepend(this.menuButton);
 	this.hideMenu();
 	this.removeButtonClasses();
 	this.menu.attr('role', 'menu');
-	this.container.find('.moj-button-menu__item').attr('role', 'menuitem');
+	this.container.find('.app-button-menu__item').attr('role', 'menuitem');
 };
 
-MOJFrontend.ButtonMenu.prototype.enableBigMode = function() {
+AppFrontend.ButtonMenu.prototype.enableBigMode = function() {
 	this.menuButton.detach();
 	this.showMenu();
 	this.addButtonClasses();
 	this.menu.removeAttr('role');
-	this.container.find('.moj-button-menu__item').removeAttr('role');
+	this.container.find('.app-button-menu__item').removeAttr('role');
 };
 
-MOJFrontend.ButtonMenu.prototype.removeButtonClasses = function() {
-	this.menu.find('.moj-button-menu__item').each(function(index, el) {
+AppFrontend.ButtonMenu.prototype.removeButtonClasses = function() {
+	this.menu.find('.app-button-menu__item').each(function(index, el) {
 		if($(el).hasClass('govuk-button--secondary')) {
 			$(el).attr('data-secondary', 'true');
 			$(el).removeClass('govuk-button--secondary');
@@ -71,8 +71,8 @@ MOJFrontend.ButtonMenu.prototype.removeButtonClasses = function() {
 	});
 };
 
-MOJFrontend.ButtonMenu.prototype.addButtonClasses = function() {
-	this.menu.find('.moj-button-menu__item').each(function(index, el) {
+AppFrontend.ButtonMenu.prototype.addButtonClasses = function() {
+	this.menu.find('.app-button-menu__item').each(function(index, el) {
 		if($(el).attr('data-secondary') == 'true') {
 			$(el).addClass('govuk-button--secondary');
 		}
@@ -83,19 +83,19 @@ MOJFrontend.ButtonMenu.prototype.addButtonClasses = function() {
 	});
 };
 
-MOJFrontend.ButtonMenu.prototype.hideMenu = function() {
+AppFrontend.ButtonMenu.prototype.hideMenu = function() {
 	this.menuButton.attr('aria-expanded', 'false');
 };
 
-MOJFrontend.ButtonMenu.prototype.showMenu = function() {
+AppFrontend.ButtonMenu.prototype.showMenu = function() {
 	this.menuButton.attr('aria-expanded', 'true');
 };
 
-MOJFrontend.ButtonMenu.prototype.onMenuButtonClick = function() {
+AppFrontend.ButtonMenu.prototype.onMenuButtonClick = function() {
 	this.toggle();
 };
 
-MOJFrontend.ButtonMenu.prototype.toggle = function() {
+AppFrontend.ButtonMenu.prototype.toggle = function() {
 	if(this.menuButton.attr('aria-expanded') == 'false') {
 		this.showMenu();
 		this.menu.find('[role=menuitem]').first().focus();
@@ -105,7 +105,7 @@ MOJFrontend.ButtonMenu.prototype.toggle = function() {
 	}
 };
 
-MOJFrontend.ButtonMenu.prototype.onMenuKeyDown = function(e) {
+AppFrontend.ButtonMenu.prototype.onMenuKeyDown = function(e) {
 	switch (e.keyCode) {
 		case this.keys.down:
 			this.toggle();
@@ -113,7 +113,7 @@ MOJFrontend.ButtonMenu.prototype.onMenuKeyDown = function(e) {
 	}
 };
 
-MOJFrontend.ButtonMenu.prototype.onButtonKeydown = function(e) {
+AppFrontend.ButtonMenu.prototype.onButtonKeydown = function(e) {
 	switch (e.keyCode) {
 		case this.keys.up:
 			e.preventDefault();
@@ -136,7 +136,7 @@ MOJFrontend.ButtonMenu.prototype.onButtonKeydown = function(e) {
 	}
 };
 
-MOJFrontend.ButtonMenu.prototype.focusNext = function(currentButton) {
+AppFrontend.ButtonMenu.prototype.focusNext = function(currentButton) {
 	var next = $(currentButton).next();
 	if(next[0]) {
 		next.focus();
@@ -145,7 +145,7 @@ MOJFrontend.ButtonMenu.prototype.focusNext = function(currentButton) {
 	}
 };
 
-MOJFrontend.ButtonMenu.prototype.focusPrevious = function(currentButton) {
+AppFrontend.ButtonMenu.prototype.focusPrevious = function(currentButton) {
 	var prev = $(currentButton).prev();
 	if(prev[0]) {
 		prev.focus();
